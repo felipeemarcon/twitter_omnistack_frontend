48033de394ccc59b835495138fde3d017433c4a4
Felipe Marcon <felipeemarcon@gmail.com>
2019-02-06 19:44:47 -0200

    Form login work - redirect to Timeline

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,13 +4,37 @@ import twitterLogo from '../twitter.svg'
 import './Login.css';
 
 export default class Login extends Component {
+
+  state = {
+    username: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem('@GoTwitter:username', username);
+
+    this.props.history.push('/timeline');
+    
+  }
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
     return (
       <div className="login-wrapper">
         <img src={twitterLogo} alt="GoTwitter"/>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input 
             type="text"
+            value={this.state.username}
+            onChange={this.handleInputChange}
             placeholder="Nome de usuário"
           />
           <button type="submit">Entrar</button>
